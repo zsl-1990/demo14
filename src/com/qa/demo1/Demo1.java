ed4a0d80c093e0596518af7d6a82a6fc6d857b11
zsl-1990 <364310199@qq.com>
2020-04-14 17:15:18 +0800

    这是第二个项目

---
+++ src/com/qa/demo1/Demo1.java
@@ -0,0 +1,9 @@
+package com.qa.demo1;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("abcdeffsdfjio");
+		System.out.println("abcdeffsdfjio");
+		System.out.println("abcdeffsdfjio");
+	}
+}
